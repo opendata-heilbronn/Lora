f56a990d18532859968f49f5eae1ac5760c2c00a
Greenfrog <green@frog.pond>
2018-03-03 21:40:47 +0100

    Add DHT22 at pin 9 for demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,36 @@
 #include "Arduino.h"
 #include "RfmTTN.h"
 
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+
+#define DHTPIN		9
+#define DHTTYPE		DHT22
+DHT dht(DHTPIN, DHTTYPE);
+
 void setup()
 {
 	Serial.begin(115200);
 	Serial.println(F("Startup..."));
 	RfmTTN::init();
 	Serial.println(F("...done."));
+	dht.begin();
 }
+
+uint8_t buffer[100];
 
 void loop()
 {
-	const uint8_t data[] = {1,2,3,4,5};
+	float h = dht.readHumidity();
+	float t = dht.readTemperature();
 
-	RfmTTN::sendData(data, sizeof(data));
+	uint8_t humi = (uint8_t)h;
+	int8_t temp = (int8_t)t;
+
+	buffer[0] = humi;
+	buffer[1] = *((uint8_t*)&temp);
+
+	RfmTTN::sendData(buffer, 2);
 
 	delay(60000);
 }
