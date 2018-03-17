d293f0616e89e47925daad57cb19df1170c8a98d
Leandro Späth <leandro.spaeth@gmail.com>
2018-03-17 19:22:53 +0100

    implement basic payload sending on Arduino Micro

--- LoRa_Feinstaub_SDS011/src/config-rfm.h
+++ LoRa_Feinstaub_SDS011/src/config-rfm.h
@@ -8,8 +8,8 @@
 
 // Pin mapping
 const lmic_pinmap lmic_pins = {
-	.nss = 10,
+	.nss = 5,
 	.rxtx = LMIC_UNUSED_PIN,
-	.rst = 5,
-	.dio = {2, 3, LMIC_UNUSED_PIN},
+	.rst = 6,
+	.dio = {7, 9, 8},
 };
--- LoRa_Feinstaub_SDS011/src/main.cpp
+++ LoRa_Feinstaub_SDS011/src/main.cpp
@@ -1,36 +1,41 @@
 #include "Arduino.h"
 #include "RfmTTN.h"
 
-#include <Adafruit_Sensor.h>
-#include <DHT.h>
 
-#define DHTPIN		9
-#define DHTTYPE		DHT22
-DHT dht(DHTPIN, DHTTYPE);
+struct payload_t {
+	uint16_t pm10;
+	uint16_t pm25;
+	int16_t temp;
+	int16_t humid; 
+} payload;
 
 void setup()
 {
+	delay(5000);
 	Serial.begin(115200);
-	Serial.println(F("Startup..."));
+	Serial.println(F("LoRa Startup..."));
 	RfmTTN::init();
 	Serial.println(F("...done."));
-	dht.begin();
+	Serial.println(sizeof(payload));
 }
 
-uint8_t buffer[100];
+void debug(uint8_t *data, uint8_t size) {
+	for(int i = 0; i < size; i++) {
+		Serial.print(data[i], 16);
+		Serial.print(" ");
+	}
+	Serial.println();
+}
 
 void loop()
 {
-	float h = dht.readHumidity();
-	float t = dht.readTemperature();
+	payload.pm10 = 10;
+	payload.pm25 = 25;
+	payload.temp = 1000; //temperature times 100
+	payload.humid = 5000; //humidity times 100
 
-	uint8_t humi = (uint8_t)h;
-	int8_t temp = (int8_t)t;
-
-	buffer[0] = humi;
-	buffer[1] = *((uint8_t*)&temp);
-
-	RfmTTN::sendData(buffer, 2);
-
+	debug(reinterpret_cast<uint8_t*>(&payload), sizeof(payload));
+	RfmTTN::sendData(reinterpret_cast<uint8_t*>(&payload), sizeof(payload));
+	
 	delay(60000);
 }
