f3a7e783100111563ad8cf3beb1dc3f791b844ff
Leandro Späth <leandro.spaeth@gmail.com>
2018-03-18 02:14:50 +0100

    add sensor reading functionality

--- LoRa_Feinstaub_SDS011/src/main.cpp
+++ LoRa_Feinstaub_SDS011/src/main.cpp
@@ -1,41 +1,103 @@
 #include "Arduino.h"
-#include "RfmTTN.h"
+#include "SoftwareSerial.h"
+#include "Wire.h"
+#include "Adafruit_Sensor.h"
 
+#include "RfmTTN.h"
+#include <SDS011.h>
+#include <Adafruit_BMP280.h>
+
+#define SERIAL_DEBUG false
 
 struct payload_t {
 	uint16_t pm10;
 	uint16_t pm25;
-	int16_t temp;
-	int16_t humid; 
+	int16_t temp =  -30000;
+	int16_t humid = -30000; 
+	int16_t press = -30000;
 } payload;
+
+SDS011 pmSensor;
+float pm10, pm25;
+
+Adafruit_BMP280 bmp;
 
 void setup()
 {
 	delay(5000);
 	Serial.begin(115200);
-	Serial.println(F("LoRa Startup..."));
+
+	pmSensor.begin(&Serial1);
+
+
+	#if SERIAL_DEBUG
+		Serial.print(F("BMP280 Startup..."));
+	#endif
+	if(!bmp.begin(0x76)) {
+		#if SERIAL_DEBUG
+			Serial.println("ERROR: No valid BMP280 sensor found. No temperature/pressure measurements will be taken");
+		#endif
+	}
+	#if SERIAL_DEBUG
+		Serial.println(F("done."));
+
+		Serial.print(F("LoRa Startup..."));
+	#endif
 	RfmTTN::init();
-	Serial.println(F("...done."));
-	Serial.println(sizeof(payload));
+	#if SERIAL_DEBUG
+		Serial.println(F("done."));
+	#endif
 }
 
-void debug(uint8_t *data, uint8_t size) {
-	for(int i = 0; i < size; i++) {
-		Serial.print(data[i], 16);
-		Serial.print(" ");
+#if SERIAL_DEBUG
+	void debug(uint8_t *data, uint8_t size) {
+		for(int i = 0; i < size; i++) {
+			Serial.print(data[i], 16);
+			Serial.print(" ");
+		}
+		Serial.println();
 	}
-	Serial.println();
-}
+#endif
 
 void loop()
 {
-	payload.pm10 = 10;
-	payload.pm25 = 25;
-	payload.temp = 1000; //temperature times 100
-	payload.humid = 5000; //humidity times 100
+	pmSensor.wakeup();
+	delay(15000); //TODO: replace with sleep
+	pmSensor.read(&pm25, &pm10);
+	pmSensor.sleep();
+	payload.pm10 = (int)pm10;
+	payload.pm25 = (int)pm25;
 
-	debug(reinterpret_cast<uint8_t*>(&payload), sizeof(payload));
+	#if SERIAL_DEBUG
+		Serial.print(F("PM Sensor data: 2.5µm: "));
+		Serial.print(pm25);
+		Serial.print(F("  10µm: "));
+		Serial.println(pm10);
+	#endif
+
+
+	float temp = bmp.readTemperature();
+	float pres = bmp.readPressure();
+
+	if(temp > -139 && pres < 128000 && pres > 0) {
+		payload.temp  = temp * 100; //temperature times 100
+		payload.press = pres / 100; //pressure in hpa
+
+		#if SERIAL_DEBUG
+			Serial.print("Temperature = ");
+			Serial.print(bmp.readTemperature());
+			Serial.println(" *C");
+			
+			Serial.print("Pressure = ");
+			Serial.print(bmp.readPressure());
+			Serial.println(" Pa");
+		#endif
+	}
+
+	#if SERIAL_DEBUG
+		debug(reinterpret_cast<uint8_t*>(&payload), sizeof(payload));
+	#endif
 	RfmTTN::sendData(reinterpret_cast<uint8_t*>(&payload), sizeof(payload));
 	
-	delay(60000);
+	delay((1 * 60 * 1000L) - 15000); //TODO: replace with sleep
 }
--- README.md
+++ README.md
@@ -2,3 +2,28 @@
 
 ## LMIC_improved_example
 Easier to use code for LMIC library for the RFM95 module.
+
+## LoRa_Feinstaub_SDS011
+### Hardware
+- Arduino Micro
+- RFM95 LoRa module
+- SDS011 particulate matter sensor
+- BMP280 temperature and pressure sensor
+### Notes
+- Uses the TTN to Luftdaten forwarder script from [Cinezaster](https://github.com/Cinezaster/ttn2luftdaten_forwarder)
+  - This forwarder creates the Luftdaten device ID with the pattern [prefix]-[deviceEUI-in-decimal]
+  - Using the following `docker-compose.yaml` file:
+  ```yaml
+    version: '3'
+    services:
+        ttn2luftdaten_forwarder:
+            environment:
+                - appID=xxx
+                - accessKey=xxx
+                - prefix=xxx
+            image: 'cinezaster/ttn2luftdaten_forwarder:latest'
+  ```
+   - appID, accessKey are copied from the TTN application, prefix is your own choice (we used `cfh`)
+ - Connect BMP280 to Arduino Micro Pins 2 (SDA) and 3 (SCL)
+   - maybe need to change address in bmp.begin() to `0x77` depending on your wiring
+ - Connect SDS011 to Arduino Micro hardware serial pins (TX and RX pins on pin header)
